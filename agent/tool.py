3b86eee233ff60d0d38aa0ed78cf0c982349bd29
Bianca Tamayo <bianca@btamayo.me>
2024-06-08 15:03:13 -0700

    Add additional filtering to `HomeSearchResultsTool` tool  (#17)
    * Add other parameters and basic logging config
    
    * Add additional parameters, remove CSV parsing
    
    * Update max results param
    
    * add basic filters
    
    * Update
    
    * Update

--- agent/tool.py
+++ agent/tool.py
@@ -1,4 +1,7 @@
 from enum import Enum
+import logging
+import os
+from pathlib import Path
 from typing import Type, Optional, Literal
 from string import Template
 from pandas import DataFrame
@@ -9,14 +12,50 @@ from langchain_core.callbacks import CallbackManagerForToolRun
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_core.tools import BaseTool
 
-# Debug
-from langchain.globals import set_verbose, set_debug
 
-from agent.listingscraper.parse import (
-    expand_to_csv_row,
-    format_human_readable,
-    save_row_data,
+human_readable_row_tpl = Template(
+    """
+---
+$mls_id: $street, $city, $zip_code
+
+property style: $style
+street: $street
+city: $city
+zip Code: $zip_code
+bedrooms: $beds bedrooms
+stories: $stories stories
+full baths: $full_baths
+half baths: $half_baths
+sqft: $sqft sqft
+listed price: $list_price
+sold for: $sold_price
+nearby schools: $nearby_schools
+listing url: $property_url
+
+description: $text
+---
+"""
 )
+
+
+def format_human_readable(property: DataFrame) -> str:
+    return human_readable_row_tpl.substitute(property.to_dict())
+
+
+def save_rows_to_csv(properties: DataFrame, filename: str = "listings.csv") -> str:
+    print(f"Saving rows data to {filename}...")
+
+    # todo: update path access to be cleaner
+    data_base_dir_path = Path(os.getcwd(), "data")
+    listings_csv_path = Path(data_base_dir_path, filename)
+
+    if not data_base_dir_path.exists():
+        data_base_dir_path.mkdir()
+
+    if not listings_csv_path.exists():
+        listings_csv_path.touch()
+
+    properties.to_csv(listings_csv_path)
 
 
 class ListingType(Enum):
@@ -33,7 +72,25 @@ class HomeSearchResultsInput(BaseModel):
     listing_type: ListingType = Field(
         default=ListingType.FOR_SALE, description="type of listing"
     )
-    radius: Optional[float] = None
+    radius: Optional[float] = Field(
+        description="""
+        Radius in miles to find comparable properties based on individual addresses. Example: 5.5 (fetches properties within a 5.5-mile radius if location is set."""
+    )
+
+    # Everything below is a param to the Tool, but not to the homeharvest scraper
+    bedroom_number: Optional[int] = Field(
+        description="""The number of bedrooms a user is looking for in a property. If not provided, it defaults to 2.0.
+    """
+    )
+    bathroom_number: Optional[float] = Field(
+        description="""The number of bathrooms a user is looking for in a property. If not provided, it defaults to 2.0."""
+    )
+    min_price: Optional[int] = Field(
+        description="""The minimum price of a property to search for in United States Dollars. If not provided, it defaults to 10000000"""
+    )
+    max_price: Optional[int] = Field(
+        description="""The maximum price of a property to search for in United States Dollars. If not provided, it defaults to 100000000"""
+    )
 
 
 # This subclasses langchain's BaseTool to create a custom Tool to pass into OpenAI.
@@ -99,13 +156,19 @@ class HomeSearchResultsTool(BaseTool):
     description: str = (
         "Uses homeharvest as a result fetching tool for the real state market"
     )
-    max_results: int = 5
+
+    max_results: int = 20
     args_schema: Type[BaseModel] = HomeSearchResultsInput
 
     def _run(
         self,
         location: str,
         listing_type: Optional[ListingType] = "FOR_SALE",
+        # todo: what happens if we input a float?
+        min_price: Optional[int] = 10000000,
+        max_price: Optional[int] = 100000000,
+        bedroom_number: Optional[int] = 2.0,
+        bathroom_number: Optional[float] = 2.0,
         radius: Optional[float] = 5.0,
         run_manager: Optional[CallbackManagerForToolRun] = None,
     ) -> str:
@@ -113,7 +176,7 @@ class HomeSearchResultsTool(BaseTool):
             if listing_type is None:
                 listing_type = ListingType.FOR_SALE
 
-            # TODO (btamayo): Validate the inputs here so that we have proper use of API calls. Sometimes 
+            # TODO (btamayo): Validate the inputs here so that we have proper use of API calls. Sometimes
             # it does not "correctly" use or infer the "right" inputs.
 
             properties = scrape_property(
@@ -121,10 +184,26 @@ class HomeSearchResultsTool(BaseTool):
             )
 
             print(properties)
+            save_rows_to_csv(properties)
+
+            logging.info("Inferred location for location: %s", location)
+            logging.info("Inferred min_price for filtering: %s", min_price)
+            logging.info("Inferred max_price for filtering: %s", max_price)
+            logging.info("Inferred bedroom_number for filtering: %s", bedroom_number)
+            logging.info("Inferred bathroom_number for filtering: %s", bathroom_number)
+            logging.info("Inferred listing_type for filtering: %s", listing_type)
+            logging.info("Inferred radius for radius: %s", radius)
+
+            res_df = properties[properties["list_price"] <= max_price]
+            res_df = res_df[res_df["list_price"] >= min_price]
+            res_df = res_df[res_df["beds"] >= bedroom_number]
+
+            # not sure how the LLM is going to parse this, but we'll see
+            res_df["bathrooms"] = res_df["full_baths"] + (res_df["half_baths"] * 0.5)
+            res_df = res_df[res_df["bathrooms"] >= bathroom_number]
 
             properties_expanded = []
-            for _, row in properties.iloc[0:self.max_results].iterrows():
-                #save_row_data(row)
+            for _, row in res_df.iloc[0 : self.max_results].iterrows():
                 expanded_row = format_human_readable(row)
                 properties_expanded.append(expanded_row)
         except Exception as e:
--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+import logging
 import os
+import sys
 import chainlit as cl
 import traceback
 
@@ -17,20 +19,25 @@ from langchain_openai import ChatOpenAI
 
 from agent.tool import HomeSearchResultsTool
 
+logging.getLogger("urllib3").setLevel(logging.CRITICAL)
+logging.basicConfig(
+    stream=sys.stderr,
+    level=logging.DEBUG,
+    format="%(module)s - %(filename)s - %(levelname)s - %(message)s",
+)
+
 load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
+langchain_api_key = os.getenv("LANGCHAIN_API_KEY", "")
+langchain_prompt_name = os.getenv("LANGCHAIN_PROMPT", "hwchase17/structured-chat-agent")
 
 # Setting up conversational memory
 conversational_memory = ConversationBufferWindowMemory(
     memory_key="chat_history", k=5, return_messages=True
 )
 
-tools = [HomeSearchResultsTool(max_results=5)]
-prompt = hub.pull("hwchase17/structured-chat-agent") 
-
-def _handle_error(error) -> str:
-    print(f"\n\nError: {error}")
-    return str(error)
+tools = [HomeSearchResultsTool(max_results=20)]
+prompt = hub.pull(langchain_prompt_name, api_key=langchain_api_key)
 
 
 @cl.on_chat_start
@@ -47,7 +54,7 @@ async def on_chat_start():
         agent=agent,
         tools=tools,
         verbose=True,
-        handle_parsing_errors=False,
+        handle_parsing_errors=True,
         memory=conversational_memory,
         return_intermediate_steps=True,
     )
@@ -57,16 +64,19 @@ async def on_chat_start():
 @cl.on_message
 async def on_message(message: cl.Message):
     agent_executor = cl.user_session.get("agent_executor")
-
-    response = agent_executor.invoke({"input": message.content})
-    intermediate_steps = response["intermediate_steps"]
-    tool_messages = [i[1] for i in intermediate_steps if i[0].tool == "home_search_results_tool"]
-    
-    for message in tool_messages:
-        conversational_memory.chat_memory.add_messages(
-            [HumanMessage(type="human", content="Can you give me detailed information about the properties?"), AIMessage(content=message)]
-        )
+    try:
+        response = agent_executor.invoke({"input": message.content})
+        intermediate_steps = response["intermediate_steps"]
+        tool_messages = [i[1] for i in intermediate_steps if i[0].tool == "home_search_results_tool"]
+        
+        for message in tool_messages:
+            conversational_memory.chat_memory.add_messages(
+                [HumanMessage(type="human", content="Can you give me detailed information about the properties?"), AIMessage(content=message)]
+            )
+    except Exception as ex:
+        print(f"\n\nError: {ex}\n\n")
+        print("\n\nError Stacktrace: \n\n")
+        traceback.print_stack()
     
     msg = cl.Message(content=response["output"])
-    
     await msg.send()  
